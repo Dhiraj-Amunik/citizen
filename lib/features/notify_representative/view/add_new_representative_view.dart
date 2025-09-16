0f9a157e58ca883eb6dd514f9a65f12f8d5e2aa3
Dhiraj-Amunik <dhiraj.amunik@gmail.com>
2025-09-16 10:19:17 +0530

    notify_setup

---
+++ lib/features/notify_representative/view/add_new_representative_view.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AddNewRepresentativeView extends StatelessWidget {
+  const AddNewRepresentativeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
